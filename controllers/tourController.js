d56a9cacbbd2989577163340c75ae624cb49b005
catskillet.tserafim <catskillet.tserafim@hubfintech.com.br>
2021-10-07 21:57:04 -0300

    Add mongodb and refactor tourController and tourRoutes

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,85 +1,65 @@
-const fs = require('fs');
+const Tour = require('../models/tourModel');
 
 // 1 - Route Handlers
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
-
-exports.checkID = (req, res, next, val) => {
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'Fail',
-            message: 'Invalid ID'
-        });
-    }
-    next();
-}
-
 exports.checkBody = (req, res, next) => {
-    if (!req.body.name || !req.body.price) {
-        return res.status(400).json({
-            status: 'Fail',
-            message: 'Missing name or price'
-        });
-    }
-    next();
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'Fail',
+      message: 'Missing name or price',
+    });
+  }
+  next();
 };
 
 exports.getAllTours = (req, res) => {
-    console.log(req.requestTime)
+  console.log(req.requestTime);
 
-    res.status(200).json({
-        status: 'success',
-        requestedAt: req.requestTime,
-        results: tours.length,
-        data: {
-            tours
-        }
-    });
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    // results: tours.length,
+    // data: {
+    //   tours,
+    // },
+  });
 };
 
 exports.getTour = (req, res) => {
-    // console.log(req.params);
+  // console.log(req.params);
 
-    const id = req.params.id * 1;
-    const tour = tours.find(el => el.id === id);
+  const id = req.params.id * 1;
+  // const tour = tours.find((el) => el.id === id);
 
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour
-        }
-    });
-}
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: {
+  //     tour,
+  //   },
+  // });
+};
 
 exports.createTour = (req, res) => {
-    // console.log(req.body);
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({ id: newId }, req.body);
-
-    tours.push(newTour);
-
-    fs.writeFile(`${__dirname}/../dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tour: newTour
-            }
-        });
-    });
+  res.status(201).json({
+    status: 'success',
+    // data: {
+    //   tour: newTour,
+    // },
+  });
 };
 
 exports.updateTour = (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: '<Update tour here.>'
-        }
-    });
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Update tour here.>',
+    },
+  });
 };
 
 exports.deleteTour = (req, res) => {
-    res.status(204).json({
-        status: 'success',
-        data: null
-    });
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
 };
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,25 +1,24 @@
 const express = require('express');
-const tourController = require('./../controllers/tourController');
+const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 
-router.param('id', tourController.checkID);
+// router.param('id', tourController.checkID);
 
 /// Create a checkBody middleware
 // Check if body contains the name and price property
 // If not, send back 400 (bad request)
 // Add it to the post handlers stack
 
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.checkBody, tourController.createTour);
 
-
-
-router.route('/')
-    .get(tourController.getAllTours)
-    .post(tourController.checkBody, tourController.createTour)
-
-router.route('/:id')
-    .get(tourController.getTour)
-    .patch(tourController.updateTour)
-    .delete(tourController.deleteTour)
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
 
 module.exports = router;
