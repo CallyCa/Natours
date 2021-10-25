ac2e779aba94490411d334078231cdc73faa500f
callyca <blackyrite@hotmail.com>
2021-10-24 22:31:01 -0300

    Modelling users

---
+++ models/userModel.js
@@ -0,0 +1,32 @@
+const moongose = require('mongoose');
+const validator = require('validator');
+
+// TODO create new modelUser with name, email, photo, password and password confirm;
+
+const userSchema = new moongose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+    validator: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+  },
+});
+
+const User = moongose.model('User', userSchema);
+
+module.exports = User;
