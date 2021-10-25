706e944d005d2f26759b706920ed05e57fe3ccab
callyca <blackyrite@hotmail.com>
2021-10-25 12:20:33 -0300

    Created authentication controller

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,6 @@
 const moongose = require('mongoose');
 const validator = require('validator');
-
-// TODO create new modelUser with name, email, photo, password and password confirm;
+const bcrypt = require('bcryptjs');
 
 const userSchema = new moongose.Schema({
   name: {
@@ -13,7 +12,7 @@ const userSchema = new moongose.Schema({
     required: [true, 'Please provide your email'],
     unique: true,
     lowercase: true,
-    validator: [validator.isEmail, 'Please provide a valid email'],
+    validate: [validator.isEmail, 'Please provide a valid email'],
   },
   photo: String,
   password: {
@@ -24,7 +23,27 @@ const userSchema = new moongose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
+    validate: {
+      // This only works on CREATE and SAVE
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!',
+    },
   },
+});
+
+// Passwords required to be encrypted with bcrypt
+
+userSchema.pre('save', async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+
+  next();
 });
 
 const User = moongose.model('User', userSchema);
