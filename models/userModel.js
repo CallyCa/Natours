5a06a3c55f7fb99d85771202a613a5d322e24962
callyca <blackyrite@hotmail.com>
2021-11-24 22:44:46 -0300

    password reset and functionality reset token

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,7 @@
 const moongose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
 
 const userSchema = new moongose.Schema({
   name: {
@@ -38,6 +39,8 @@ const userSchema = new moongose.Schema({
     },
   },
   passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
 });
 
 // Passwords required to be encrypted with bcrypt
@@ -70,6 +73,16 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
   }
 
   return false;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  crypto.createHash('sha256').update(resetToken).digest('hex');
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
 };
 
 const User = moongose.model('User', userSchema);
