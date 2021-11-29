5b7836eb97f04bf42526030e96fcf44fca09ef30
callyca <blackyrite@hotmail.com>
2021-11-28 22:41:34 -0300

    Sending email with nodemailer

---
+++ utils/email.js
@@ -0,0 +1,30 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (options) => {
+  // TODO Create a transporter
+
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  // TODO Define the email address
+
+  const mailOptions = {
+    from: 'CallyDev <devCally@developer.io>',
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    //   html:
+  };
+
+  // TODO Actually send the email
+
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
