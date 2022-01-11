78dee1c381e02e6a4f7ec0cdccc579d805fe33fd
callyca <blackyrite@hotmail.com>
2022-01-10 22:05:33 -0300

    adding a nested get endpoint

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
-const reviewController = require('../controllers/reviewController');
+const reviewRouter = require('./reviewRoutes');
 
 const router = express.Router();
 
@@ -11,6 +11,8 @@ const router = express.Router();
 // Check if body contains the name and price property
 // If not, send back 400 (bad request)
 // Add it to the post handlers stack
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router
   .route('/top-5-cheap')
@@ -34,12 +36,12 @@ router
     tourController.deleteTour
   );
 
-router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protect,
-    authController.restrictTo('user'),
-    reviewController.createReview
-  );
+// router
+//   .route('/:tourId/reviews')
+//   .post(
+//     authController.protect,
+//     authController.restrictTo('user'),
+//     reviewController.createReview
+//   );
 
 module.exports = router;
